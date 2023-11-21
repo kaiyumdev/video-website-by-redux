3a2d5c4d44f2ae4541b7bb7fd6735b48ac75cbe1
MdKaioumislam5 <afrankaiyum34@gmail.com>
2023-11-21 11:40:05 +0600

    get query-single video

--- src/components/pages/Video.js
+++ src/components/pages/Video.js
@@ -3,27 +3,55 @@ import { useGetVideoQuery } from "../../features/api/apiSlice";
 import Description from "../video/Description";
 import Player from "../video/Player";
 import RelatedVideos from "../video/related/RelatedVideos";
+import PlayerLoader from "../ui/loaders/PlayerLoader";
+import DescriptionLoader from "../ui/loaders/DescriptionLoader";
+import Error from "../ui/Error";
+import RelatedVideoLoader from "../ui/loaders/RelatedVideoLoader";
 
 export default function Video() {
   const { videoId } = useParams();
   const { data: video, isLoading, isError } = useGetVideoQuery(videoId);
 
   let content = null;
-  if (isLoading)
+  if (isLoading) {
     content = (
       <>
-        <Player />
-        <Description />
+        <PlayerLoader></PlayerLoader>
+        <DescriptionLoader></DescriptionLoader>
       </>
     );
+  }
+
+  if (!isLoading && isError) {
+    content = <Error message="There was an error occurred!"></Error>;
+  }
+
+  if (!isLoading && !isError && video?.id) {
+    content = (
+      <>
+        <Player link={video.link} title={video.title} />
+        <Description video={video} />
+      </>
+    );
+  }
 
   return (
     <section className="pt-6 pb-20 min-h-[calc(100vh_-_157px)]">
       <div className="mx-auto max-w-7xl px-2 pb-20 min-h-[400px]">
         <div className="grid grid-cols-3 gap-2 lg:gap-8">
-          <div className="col-span-full w-full space-y-8 lg:col-span-2"></div>
-
-          <RelatedVideos />
+          <div className="col-span-full w-full space-y-8 lg:col-span-2">
+            {content}
+          </div>
+          {video?.id ? (
+            <RelatedVideos />
+          ) : isLoading ? (
+            <>
+              {" "}
+              <RelatedVideoLoader></RelatedVideoLoader>{" "}
+            </>
+          ) : (
+            <Error message="There was an error occurred"></Error>
+          )}
         </div>
       </div>
     </section>
