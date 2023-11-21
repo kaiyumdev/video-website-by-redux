175f159b963b754a8177f08c569d08932c73fa1f
MdKaioumislam5 <afrankaiyum34@gmail.com>
2023-11-21 11:06:28 +0600

    create first slice

--- src/app/store.js
+++ src/app/store.js
@@ -5,4 +5,7 @@ export const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
   },
+
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
 });
