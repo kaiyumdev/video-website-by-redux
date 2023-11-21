dffb8781ad2dc047b38a7eda31347c71baf3f830
MdKaioumislam5 <afrankaiyum34@gmail.com>
2023-11-21 23:21:07 +0600

    adding video -post request

--- src/components/add/Form.js
+++ src/components/add/Form.js
@@ -1,57 +1,131 @@
 // import Success from "../ui/Success";
 import TextArea from "../ui/TextArea";
 import TextInput from "../ui/TextInput";
+import Success from "../ui/Success";
+import Error from "../ui/Error";
+import { useState } from "react";
+import { useAddVideoMutation } from "../../features/api/apiSlice";
 
 export default function Form() {
-    return (
-        <form action="#" method="POST">
-            <div className="shadow overflow-hidden sm:rounded-md">
-                <div className="px-4 py-5 bg-white sm:p-6">
-                    <div className="grid grid-cols-6 gap-6">
-                        <div className="col-span-6 sm:col-span-3">
-                            <TextInput title="Video Title" />
-                        </div>
+  const [AddVideo, { isLoading, isError, isSuccess }] = useAddVideoMutation();
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const [description, setDescription] = useState("");
+  const [link, setLink] = useState("");
+  const [thumbnail, setThumbnail] = useState("");
+  const [date, setDate] = useState("");
+  const [duration, setDuration] = useState("");
+  const [views, setViews] = useState("");
 
-                        <div className="col-span-6 sm:col-span-3">
-                            <TextInput title="Author" />
-                        </div>
+  const reset = () => {
+    setTitle("");
+    setAuthor("");
+    setDescription("");
+    setLink("");
+    setThumbnail("");
+    setDate("");
+    setDuration("");
+    setViews("");
+  };
 
-                        <div className="col-span-6">
-                            <TextArea title="Description" />
-                        </div>
-
-                        <div className="col-span-6">
-                            <TextInput title="YouTube Video link" />
-                        </div>
-
-                        <div className="col-span-6">
-                            <TextInput title="Thumbnail link" />
-                        </div>
-
-                        <div className="col-span-6 sm:col-span-6 lg:col-span-2">
-                            <TextInput title="Upload Date" />
-                        </div>
-
-                        <div className="col-span-6 sm:col-span-3 lg:col-span-2">
-                            <TextInput title="Video Duration" />
-                        </div>
-
-                        <div className="col-span-6 sm:col-span-3 lg:col-span-2">
-                            <TextInput title="Video no of views" />
-                        </div>
-                    </div>
-                </div>
-                <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
-                    <button
-                        type="submit"
-                        className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-0 focus:ring-offset-0 focus:ring-indigo-500"
-                    >
-                        Save
-                    </button>
-                </div>
-
-                {/* <Success message="Video was added successfully" /> */}
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    AddVideo({
+      title,
+      author,
+      description,
+      link,
+      thumbnail,
+      date,
+      duration,
+      views,
+    });
+    reset();
+  };
+  return (
+    <form method="POST" onSubmit={handleSubmit}>
+      <div className="shadow overflow-hidden sm:rounded-md">
+        <div className="px-4 py-5 bg-white sm:p-6">
+          <div className="grid grid-cols-6 gap-6">
+            <div className="col-span-6 sm:col-span-3">
+              <TextInput
+                title="Video Title"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+              />
             </div>
-        </form>
-    );
+
+            <div className="col-span-6 sm:col-span-3">
+              <TextInput
+                title="Author"
+                value={author}
+                onChange={(e) => setAuthor(e.target.value)}
+              />
+            </div>
+
+            <div className="col-span-6">
+              <TextArea
+                title="Description"
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+              />
+            </div>
+
+            <div className="col-span-6">
+              <TextInput
+                title="YouTube Video link"
+                value={link}
+                onChange={(e) => setLink(e.target.value)}
+              />
+            </div>
+
+            <div className="col-span-6">
+              <TextInput
+                title="Thumbnail link"
+                value={thumbnail}
+                onChange={(e) => setThumbnail(e.target.value)}
+              />
+            </div>
+
+            <div className="col-span-6 sm:col-span-6 lg:col-span-2">
+              <TextInput
+                title="Upload Date"
+                value={date}
+                onChange={(e) => setDate(e.target.value)}
+              />
+            </div>
+
+            <div className="col-span-6 sm:col-span-3 lg:col-span-2">
+              <TextInput
+                title="Video Duration"
+                value={duration}
+                onChange={(e) => setDuration(e.target.value)}
+              />
+            </div>
+
+            <div className="col-span-6 sm:col-span-3 lg:col-span-2">
+              <TextInput
+                title="Video no of views"
+                value={views}
+                onChange={(e) => setViews(e.target.value)}
+              />
+            </div>
+          </div>
+        </div>
+        <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
+          <button
+            type="submit"
+            className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-0 focus:ring-offset-0 focus:ring-indigo-500"
+          >
+            Save
+          </button>
+        </div>
+
+        {isSuccess && (
+          <Success message="video was added successfully"></Success>
+        )}
+        {isError && <Error message="There was an error adding video"></Error>}
+      </div>
+    </form>
+  );
 }
